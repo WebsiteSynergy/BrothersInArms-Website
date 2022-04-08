2367207cfd3f5b97da68a328c3fc6a34d9b824d8
ar00n <asimpole.as@gmail.com>
2022-04-08 17:08:37 +0100

    As far as i got

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,78 @@
-const path = require('path');
+module.exports = ({ mode }) => {
+  const pathToMainJs = require.resolve("./src/main.js");
+  const pathToIndexHtml = require.resolve("./src/index.html");
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-
- module: {
-   rules: [
-    {
-        test: /\.html$/,
-        use: [
-            'file-loader?name=[name].[ext]', 
-            'extract-loader', 
-            {
-                loader: 'html-loader',
-                options: {
-                  esModule: false,
-                }
-            },
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-     {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-       type: 'asset/resource',
-     },      
-   ],
- },
-};
+  return {
+      mode,
+      entry: [
+          pathToMainJs,
+          pathToIndexHtml
+      ],
+      module: {
+          rules: [
+              {
+                  test: pathToIndexHtml,
+                  use: [
+                      "file-loader",
+                      "extract-loader",
+                      {
+                          loader: "html-loader",
+                          options: {
+                            esModule: false,
+                            sources: {
+                              list: [
+                                // All default supported tags and attributes
+                                "...",
+                                {
+                                  tag: "img",
+                                  attribute: "data-src",
+                                  type: "src",
+                                },
+                                {
+                                  tag: "img",
+                                  attribute: "data-srcset",
+                                  type: "srcset",
+                                },
+                              ],
+                              urlFilter: (attribute, value, resourcePath) => {
+                                // The `attribute` argument contains a name of the HTML attribute.
+                                // The `value` argument contains a value of the HTML attribute.
+                                // The `resourcePath` argument contains a path to the loaded HTML file.
+                  
+                                if (/\.svg$/.test(value)) {
+                                  return false;
+                                }
+                  
+                                return true;
+                              },
+                            }
+                          }
+                      }
+                  ]
+              },
+              {
+                  test: /\.css$/,
+                  use: [
+                      "file-loader",
+                      "extract-loader",
+                      {
+                          loader: "css-loader",
+                          options: {
+                              sourceMap: true,
+                              esModule: false,
+                          }
+                      }
+                  ]
+              },
+              {
+                  test: /\.jpg$/,
+                  use: "file-loader"
+              },
+              {
+                test: /\.svg$/,
+                type: "asset/resource"
+            }
+          ]
+      }
+  };
+}
